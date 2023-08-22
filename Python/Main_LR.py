f68a6af2325432adbc5ba03ed599d94a78653b11
ValentinLAPLAUD <vallap.biophysics@protonmail.com>
2023-08-22 16:24:56 +0200

    P :Added averaging function
    Averages weather data over several month and from different DF, results is all merged in one DF

--- Python/Main_LR.py
+++ Python/Main_LR.py
@@ -25,3 +25,28 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
+
+def MonthAvg(dfs,AllVars,monthlist):
+    
+    dffinal = pd.DataFrame(columns=['STATION','ALTITUDE','LATITUDE','LONGITUDE','M_List'])
+
+    for df,Vars in zip(dfs,AllVars):
+
+        # Month selection
+        dfmonth = df.loc[vf.ismember(monthlist,df['MONTH'].values)].copy(deep=True)
+
+        StationList = np.unique(df['STATION'])
+
+        for Var in Vars:
+            for S in StationList:
+
+                dfmonth.loc[(dfmonth['STATION'] == S)&(dfmonth['MONTH'] == monthlist[0]),Var+'_AVG'] = np.nanmean(dfmonth.loc[df['STATION'] == S][Var])
+                dfmonth.loc[(dfmonth['STATION'] == S)&(dfmonth['MONTH'] == monthlist[0]),'M_List'] = str(monthlist)
+        dffinal = dfmonth.loc[dfmonth['MONTH'] == monthlist[0],
+                                                  ['STATION','ALTITUDE','LATITUDE','LONGITUDE','M_List']+
+                                                  [ V + '_AVG' for V in Vars]].merge(dffinal,how='outer',
+                                                                                     on=['STATION','ALTITUDE','LATITUDE','LONGITUDE','M_List'])
+
+    return dffinal
+
+    
